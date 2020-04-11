ee4b94844fc10686254f5802b32debf934102646
Lucas Moreira <nirvana.lucasmoreira@gmail.com>
2020-04-11 18:53:37 -0300

    :construction: Insere códigos para inicializar um jogo da memória

---
+++ index.js
@@ -0,0 +1,14 @@
+//Função que será chamada quando a tela for carregada
+function onLoad() {    
+    const dependencias = {
+        //A classe Tela é uma dependência e será utilizada como parâmetro quando instanciar um jogo da memória, por isso criar um objeto chamado dependencias
+        tela: Tela
+    }
+
+    //Instancia um novo jogo da memória passando dependencias como parâmetro
+    const jogoDaMemoria = new JogoDaMemoria(dependencias)
+    jogoDaMemoria.inicializar()
+}
+
+//Faz com que a variável global responsável pelo carregamento da tela, windows.onload receba a função criada acima, onLoad
+window.onload = onLoad
